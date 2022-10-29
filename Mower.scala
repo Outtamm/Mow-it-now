40760d54338ac4ae9a83432085069923f0e4a91c
Outtamm <116350086+Outtamm@users.noreply.github.com>
2022-10-29 15:39:48 +0200

    Create Mower.scala

---
+++ Mower.scala
@@ -0,0 +1,49 @@
+case class Mower(position : Coordonnees, direction : Direction) {
+  def tournerADroite(): Mower = {
+    Mower(this.position, this.direction.directionOnTheRight())
+  }
+
+  def tournerAGauche(): Mower = {
+    Mower(this.position, this.direction.directionOnTheLeft())
+  }
+
+  def getX(): Int = {
+    this.position.x
+  }
+
+  def getY(): Int = {
+    this.position.y
+  }
+
+  def avancer(jardin: Jardin): Unit = {
+    val newCoordinate = this.direction match {
+      case North => Coordonnees(this.position.x, this.position.y + 1)
+      case East => Coordonnees(this.position.x + 1, this.position.y)
+      case South => Coordonnees(this.position.x, this.position.y - 1)
+      case West => Coordonnees(this.position.x - 1, this.position.y)
+        if (jardin.isValidCoordinate(newCoordinate)) {
+          Mower(newCoordinate, this.direction)
+        }
+        else {
+          this
+        }
+
+    }
+  }
+
+
+    def print() = this.position.x + " " + this.position.y + " " + this.direction.toString
+
+    def miseAJour(instruction: String, jardin: Jardin): Unit = {
+      instruction match {
+        case "A" => this.avancer(jardin)
+        case "D" => this.tournerADroite()
+        case "G" => this.tournerAGauche()
+        case x =>
+          throw new IllegalArgumentException(" Les seules instructions possibles sont A, D ou G")
+          this
+      }
+
+    }
+
+  }
