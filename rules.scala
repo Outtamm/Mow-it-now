2ad91ca357666c0c36e063910e0b196ab8a6c02f
Outtamm <116350086+Outtamm@users.noreply.github.com>
2022-10-31 02:50:00 +0100

    Update rules.scala

--- rules.scala
+++ rules.scala
@@ -1,20 +1,18 @@
 case class rules(mower : Mower, xMax : Int, yMax : Int) {
   def validate(): Boolean = IsInstructionPossible(this.mower)
 
-  /**
-   * Bring together all requirement with a <strong>&&</strong> operator. <br/>
-   * So to pass all requirements should be filled and return true.
+  /** Permet de savoir si l'instruction demandée est réalisable ou non
    *
    * @param m : Mower
-   * @return : Boolean weather the mower is congruent to a SquareMower
+   * @return : Booléen 
    */
   def IsInstructionPossible(m: Mower): Boolean = {
     isNorthPossible(m) && isRightPossible(m) && isLeftPossible(m) && isSouthPossible(m)
 
-    // add any additional rules here ...
+   
   }
 
-  // all the following are rules ...
+  
 
   def isNorthPossible(m: Mower): Boolean = m.getY()<=yMax
 
