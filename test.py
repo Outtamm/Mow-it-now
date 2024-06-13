4dcbf05851c6d19b2fffd4dd6daf58906bbe826c
Outtamm <116350086+Outtamm@users.noreply.github.com>
2024-06-13 12:15:42 +0200

    Ajout d'imports

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import seaborn as sns
 
 def factorielle(n):
     if n==0:
@@ -13,3 +14,8 @@ def addition(n1, n2):
 
 def soustraction(n1,n2):
     return n1-n2
+
+def multiplication(n1,n2):
+    return n1*n2
+
+
